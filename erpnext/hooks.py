e64d44ad511652a584d94f7aa1d6f36443b4379b
Alirio Castro <aliriocastro@gmail.com>
2020-03-27 00:00:48 -0400

    test1

--- erpnext/hooks.py
+++ erpnext/hooks.py
@@ -15,7 +15,7 @@ app_logo_url = '/assets/erpnext/images/erp-icon.svg'
 
 develop_version = '12.x.x-develop'
 
-app_include_js = "assets/js/erpnext.min.js"
+app_include_js = ["assets/js/erpnext.min.js"]
 app_include_css = "assets/css/erpnext.css"
 web_include_js = "assets/js/erpnext-web.min.js"
 web_include_css = "assets/css/erpnext-web.css"
