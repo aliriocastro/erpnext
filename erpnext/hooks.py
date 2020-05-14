777ea7a4c0305d4509eb71aa8c60d59e070ced45
Alirio Castro <aliriocastro@gmail.com>
2020-05-13 22:20:54 -0400

    add jenv method

--- erpnext/hooks.py
+++ erpnext/hooks.py
@@ -529,3 +529,9 @@ global_search_doctypes = {
 		{'doctype': 'Hotel Room Type', 'index': 4}
 	]
 }
+
+jenv = {
+    "methods": [
+        "get_default_ifu_by_item:labotech.api.stock.item.get_default_ifu_by_item"
+    ]
+}
