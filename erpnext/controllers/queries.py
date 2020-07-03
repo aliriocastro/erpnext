7ff042c931a855d31b74adedc9b4e4a681a45704
Alirio Castro <aliriocastro@gmail.com>
2020-07-03 15:26:31 -0400

    feat: expired batch logic

--- erpnext/controllers/queries.py
+++ erpnext/controllers/queries.py
@@ -307,9 +307,10 @@ def get_delivery_notes_to_be_billed(doctype, txt, searchfield, start, page_len, 
 
 def get_batch_no(doctype, txt, searchfield, start, page_len, filters):
 	cond = ""
-	# allow to use expired batch (depends on user)
-	# if filters.get("posting_date"):
-	#	cond = "and (batch.expiry_date is null or batch.expiry_date >= %(posting_date)s)"
+	
+	allow_expired_batches = frappe.db.get_value('Stock Settings', None, 'allow_expired_batches')
+	if filters.get("posting_date") and not allow_expired_batches:
+		cond = "and (batch.expiry_date is null or batch.expiry_date >= %(posting_date)s)"
 
 	batch_nos = None
 	args = {
--- erpnext/stock/doctype/stock_ledger_entry/stock_ledger_entry.py
+++ erpnext/stock/doctype/stock_ledger_entry/stock_ledger_entry.py
@@ -113,11 +113,14 @@ class StockLedgerEntry(Document):
 			self.posting_time = '00:00'
 
 	def validate_batch(self):
-		if self.batch_no and self.voucher_type != "Stock Entry":
-			expiry_date = frappe.db.get_value("Batch", self.batch_no, "expiry_date")
-			if expiry_date:
-				if getdate(self.posting_date) > getdate(expiry_date):
-					frappe.throw(_("Batch {0} of Item {1} has expired.").format(self.batch_no, self.item_code))
+		allow_expired_batches = frappe.db.get_value('Stock Settings', None, 'allow_expired_batches')
+
+		if not allow_expired_batches:
+			if self.batch_no and self.voucher_type != "Stock Entry":
+				expiry_date = frappe.db.get_value("Batch", self.batch_no, "expiry_date")
+				if expiry_date:
+					if getdate(self.posting_date) > getdate(expiry_date):
+						frappe.throw(_("Batch {0} of Item {1} has expired.").format(self.batch_no, self.item_code))
 
 	def validate_and_set_fiscal_year(self):
 		if not self.fiscal_year:
