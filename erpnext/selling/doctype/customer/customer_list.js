034f5180fa079fc568776c5487020eb5c38af677
Alirio Castro <aliriocastro@gmail.com>
2021-08-15 18:11:35 -0400

    refactoring: add item price combination natively

--- erpnext/selling/doctype/customer/customer_list.js
+++ erpnext/selling/doctype/customer/customer_list.js
@@ -1,3 +1,20 @@
 frappe.listview_settings['Customer'] = {
 	add_fields: ["customer_name", "territory", "customer_group", "customer_type", "image"],
+
+	onload(listview) {
+		listview.page.add_button("sample",
+			async () => {
+
+				async function execute_build_all_customers_combined_item_prices() {
+					frappe.show_progress('Reconstruyendo Lista de Precios de Todos los Clientes...', 70, 100, 'Espere, este proceso puede demorar un minuto o dos.');
+					await frappe.xcall("labotech.labotech.api.client.build_all_customers_combined_item_prices", {})
+					frappe.hide_progress();
+
+				}
+
+				await execute_build_all_customers_combined_item_prices();
+
+			}, { icon: 'setting-gear' })
+	}
 };
+
